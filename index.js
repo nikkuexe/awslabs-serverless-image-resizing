af00b2821da39f1ac9dec3e306162c0a43bcd264
Nicholas Graham <grahamni@gmail.com>
2018-01-04 16:20:02 -0800

    Limited to only 386x482, Changed format to JPG from PNG

--- index.js
+++ index.js
@@ -16,16 +16,24 @@ exports.handler = function(event, context, callback) {
   const height = parseInt(match[2], 10);
   const originalKey = match[3];
 
+	//force 386x482 size (newsletter)
+  if(width !== 386 || height !== 482){ return false; }
+  
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
       .resize(width, height)
-      .toFormat('png')
+      .background('white')
+      .embed()
+      //.toFormat('png')
+      .jpeg({ quality: 75 })
+      .toFormat('jpeg')
       .toBuffer()
     )
     .then(buffer => S3.putObject({
         Body: buffer,
         Bucket: BUCKET,
-        ContentType: 'image/png',
+        //ContentType: 'image/png',
+        ContentType: 'image/jpeg',
         Key: key,
       }).promise()
     )
